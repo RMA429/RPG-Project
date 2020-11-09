47a1390f6262c047e001039021721570b03d93eb
RMA429 <robert.alaniz01@utrgv.edu>
2020-11-09 15:49:13 -0600

    first commit

---
+++ Assets/Scripts/FollowCamera.cs
@@ -0,0 +1,15 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class FollowCamera : MonoBehaviour
+{
+    [SerializeField] Transform target = default;
+
+
+    // Update is called once per frame
+    void LateUpdate() //Late update fixes an issue of the camera moving before the player
+    {
+        transform.position = target.position;
+    }
+}
---
+++ Assets/Scripts/Mover.cs
@@ -0,0 +1,42 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.AI;
+
+public class Mover : MonoBehaviour
+{
+    [SerializeField] Transform target;
+
+   
+    // Update is called once per frame
+    void Update()
+    {
+        if (Input.GetMouseButton(0))
+        {
+            MoveToCursor();
+        }
+        UpdateAnimator();
+    }
+
+    private void MoveToCursor()
+    {
+        Ray ray = Camera.main.ScreenPointToRay(Input.mousePosition);
+        RaycastHit hit;
+        bool hasHit = Physics.Raycast(ray, out hit);
+        if(hasHit) //hasHit == true
+        {
+            GetComponent<NavMeshAgent>().destination = hit.point;
+        }
+    }
+
+    private void UpdateAnimator()
+    {
+        Vector3 velocity = GetComponent<NavMeshAgent>().velocity; //Get global Nav Mesh Velocity
+        Vector3 localVelocity = transform.InverseTransformDirection(velocity); //turn global into local
+        float speed = localVelocity.z;
+        GetComponent<Animator>().SetFloat("Forward Speed", speed);
+    }
+    
+    
+    
+}

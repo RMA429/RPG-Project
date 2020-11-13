a202bdd80c1d2344185d668ab9906d5b74a06705
RMA429 <robert.alaniz01@utrgv.edu>
2020-11-13 14:47:12 -0600

    fixed some bugs in attacking states

--- Assets/Scripts/Combat/Fighter.cs
+++ Assets/Scripts/Combat/Fighter.cs
@@ -36,17 +36,23 @@ namespace RPG.Combat
             if (timeSinceLastAttack > timeBetweenAttacks)
             {
                 //this will trigger the Hit() event
-                GetComponent<Animator>().SetTrigger("attack");
+                TriggerAttack();
                 timeSinceLastAttack = 0;
                 
             }
             
         }
 
+        private void TriggerAttack()
+        {
+            GetComponent<Animator>().ResetTrigger("stopAttack");
+            GetComponent<Animator>().SetTrigger("attack");
+        }
+
         //Animation event
         void Hit()
         {
-            
+            if (target == null) return;
             target.TakeDamage(weaponDamage);
         }
 
@@ -71,10 +77,15 @@ namespace RPG.Combat
         }
         public void Cancel()
         {
-            GetComponent<Animator>().SetTrigger("stopAttack");
+            StopAttack();
             target = null;
         }
 
+        private void StopAttack()
+        {
+            GetComponent<Animator>().ResetTrigger("attack");
+            GetComponent<Animator>().SetTrigger("stopAttack");
+        }
         
     }
 
